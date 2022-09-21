6871231930a074ef4fd7f83da26bff6705297f5f
developeruche <nweleuchenna2004@gmail.com>
2022-09-21 20:17:18 +0100

    developeruche: done writing the code for the multisig client

--- src/components/core/ActionBtn.tsx
+++ src/components/core/ActionBtn.tsx
@@ -12,7 +12,7 @@ const ActionButton: React.FC<ActionBtnProps>= ({ title, setAction, classprops })
         <span className="absolute bottom-0 right-0 w-8 h-20 -mb-8 -mr-5 transition-all duration-300 ease-out transform rotate-45 translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
         <span className="absolute top-0 left-0 w-20 h-8 -mt-1 -ml-12 transition-all duration-300 ease-out transform -rotate-45 -translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
         <span className="relative flex items-center text-sm">
-        <svg className="relative w-5 h-5 mr-2 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+        <svg className="relative w-5 h-5 mr-2 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
         {title}
         </span>
     </button>
